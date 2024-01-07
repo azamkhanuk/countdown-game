e6af12b002c6ef40485bfef93c38ad595f114c92
Azam Khan <coding@azamkhan.co.uk>
2024-01-07 16:04:06 +0000

    basic player name update with state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import {useState} from "react";
+
 export default function Player() {
+
+    const [enteredName, setEnteredName] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleChange = (event) => {
+        setSubmitted(false)
+        setEnteredName(event.target.value);
+    }
+
+    const handleClick = () => {
+        setSubmitted(true)
+    }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
