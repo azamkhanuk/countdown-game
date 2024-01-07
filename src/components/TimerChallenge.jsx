9b466c6f54b18a2699b5f55c7b24f44a46efe53d
Azam Khan <coding@azamkhan.co.uk>
2024-01-07 17:55:43 +0000

    setting up the timer challenge and conditional

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,33 @@
+import {useState} from "react";
+
 const TimerChallenge = ({title, targetTime}) => {
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+    const handleStart = () => {
+        setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime * 1000)
+        setTimerStarted(true)
+    }
+
+    const handleStop = () => {
+      
+    }
 
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>Time expired!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
             <p className="">
-                Time is running...
+                {timerStarted ? 'Time is running...' : 'Time is inactive'}
             </p>
         </section>
     );
